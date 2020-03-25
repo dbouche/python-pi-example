bedcb04077be86cdabd2b5f70faebb3fff1f1fa6
dbouche <diegobouche06@gmail.com>
2020-03-25 15:50:15 -0500

    Updated readme, added main.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-pi-example
+This is for practice. I want to learn how to become an expert in Ubuntu. Right now we are working on using git.
---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+print("Hello World!")
+if (random.randint(1,1001)%2==1):
+    print("Odd")
+else:
+    print("Pair")
